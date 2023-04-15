a4c3d2294af781005331af59ecb936c19dc9b23b
Dale.vand <advice02@naver.com>
2023-04-15 18:08:49 +0900

    wordle 붙이기 작업

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
-
 
 import uvicorn
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 class Item(BaseModel):
